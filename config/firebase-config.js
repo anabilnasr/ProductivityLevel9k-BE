2800a2a1bc2f073b3c1bbf799ba59ee59508a31b
xNebulaa <ah.nabilnasr@gmail.com>
2021-06-09 02:01:29 +0200

    backend with cors problem

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -4,6 +4,7 @@ const serviceAccount = require('./serviceAccount.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://productivitylevel9k-default-rtdb.europe-west1.firebasedatabase.app/"
 });
 
 module.exports = admin;
--- src/server.js
+++ src/server.js
@@ -1,37 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const middleware = require('../middleware/auth');
-
+const firebase = require('firebase-admin');
 const app = express();
 const port = 5000;
 
-app.use(cors());
 
+app.use(cors({ credentials: true }));
+app.use(bodyParser());
 app.use(middleware.decodeToken);
 
-app.get('/api/todos', (req, res) => {
-	return res.json({
-		tasks: [
-			{
-				title: 'Task1',
-				description: '5alas el backend ya lalaa'
-			},
-			{
-				title: 'Task2',
-				description: '5alas el frontend ya lalaa'
-			},
-			{
-				title: 'Task3',
-				description: '5alas el authentication ya lalaa'
-			},
-			{
-				title: 'Task4',
-				description: '5alas el Assignment ya lalaa'
-			},
-		],
+app.post('/tasks', cors(), (req, res) => {
+	writeUserData(req.user.uid, req.user.name, req.user.email, req.user.picture.replace('=s96', '=s150'), req.body.tasks);
+	res.status(200).json({ message: "successful" });
+	res.end();
+});
+
+app.get('/tasks', (req, res) => {
+	const ref = firebase.database().ref('users/' + req.user.uid + '/tasks');
+	ref.on('value', (tasks) => {
+		return res.json(snapshotToArray(tasks));
+	}, (errorObject) => {
+		console.log('The read failed: ' + errorObject.name);
 	});
+
 });
 
 app.listen(port, () => {
 	console.log(`server is running on port ${port}`);
 });
+function writeUserData(userId, name, email, imageUrl, tasks) {
+	const ref = firebase.database().ref('users/' + userId);
+	ref.set({
+		username: name,
+		email: email,
+		profile_picture: imageUrl,
+		tasks: tasks
+	});
+}
+function snapshotToArray(snapshot) {
+	var returnArr = [];
+
+	snapshot.forEach(function (childSnapshot) {
+		var item = childSnapshot.val();
+		item.key = childSnapshot.key;
+
+		returnArr.push(item);
+	});
+
+	return returnArr;
+};
